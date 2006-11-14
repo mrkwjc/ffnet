fc1e364c2a1cd41395d364f4ba2e95f58b36e4eb
Marek Wojciechowski <mrkwjc@users.noreply.github.com>
2006-11-14 18:02:30 +0000

    Initial import.

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from numpy.distutils.core import Extension
+
+ext1 = Extension(name = '_ffnet',
+                 sources = ['fortran/ffnet.f'])
+
+ext2 = Extension(name = '_pikaia',
+                 sources = ['fortran/pikaia.f'])
+
+if __name__ == "__main__":
+    from numpy.distutils.core import setup
+    setup(name              = 'ffnet',
+          description       = "Feed-forward neural network routines",
+          author            = "Marek Wojciechowski",
+          author_email      = "mwojc@p.lodz.pl",
+          ext_modules       = [ext1, ext2],
+          py_modules        = ["ffnet", "pikaia"]
+          )
+
