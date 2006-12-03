86ac970e71f95bc7a73b9be3d42832490f5540c7
Marek Wojciechowski <mrkwjc@users.noreply.github.com>
2006-12-03 18:37:39 +0000

    Added sin.py example. Slight changes in xor.py and ffnet.py

---
+++ examples/sin.py
@@ -0,0 +1,69 @@
+""" Sinus training example for ffnet """
+
+from ffnet import ffnet, mlgraph
+from math import pi, sin, cos
+
+# Let's create layered network connectivity by hand (architecture (1,4,1))
+# Tip: draw network on a sheet of paper and number its nodes first. :)
+# Remember that biases of nodes are handled as the connections 
+# from special node numbered 0.
+# Order of node numbering has no meaning. Order of links in conec is meaningless too,
+# but the tuples indicating connections have to be from source to target. 
+
+conec = [(1, 2), (1, 3), (1, 4), (1, 5), (2, 6), (3, 6), (4, 6), (5, 6), \
+         (0, 2), (0, 3), (0, 4), (0, 5), (0, 6)]
+
+# Note1: Adding connection from 0 to 1 will cause it not to be input node any more
+# therefore it is not allowed!
+# Note2: The same connectivity can be obtained using mlgraph function provided with ffnet.
+
+# Create network
+net = ffnet(conec)
+
+# Generate training data (sinus values from 0 to 2*pi)
+patterns = 16
+input = [ [0.] ] + [ [k*2*pi/patterns] for k in xrange(1, patterns + 1) ]
+target = [[sin(x[0])] for x in input]
+
+# Train network
+#first find good starting point with genetic algorithm (not necessary, but helpful)
+net.train_genetic(input, target, individuals=20, generations=500)
+#then train with scipy tnc optimizer
+net.train_tnc(input, target, maxfun = 10000)
+
+# Test network
+net.test(input, target, iprint = 2)
+
+# Make some plots
+try:
+    from pylab import *
+    points = 128
+    xaxis = [ 0. ] + [ k*2*pi/points for k in xrange(1, points + 1) ]
+    sinus = [ sin(x) for x in xaxis ]
+    cosinus = [ cos(x) for x in xaxis ]
+    netsinus = [ net([x]) for x in xaxis]
+    netcosinus = [ net.derivative([x]) for x in xaxis ]
+    
+    subplot(211)
+    plot(xaxis, sinus, 'b--', xaxis, netsinus, 'k-')
+    legend(('sinus', 'network output'))
+    grid(True)
+    title('Outputs of trained network.')
+    
+    subplot(212)
+    plot(xaxis, cosinus, 'b--', xaxis, netcosinus, 'k-')
+    legend(('cosinus', 'network derivative'))
+    grid(True)
+    show()
+except: 
+    print "Cannot make plots. For plotting install matplotlib..."
+
+print \
+"""
+Note:
+You have access to partial derivatives of the network
+outputs vs. its inputs. For example, calling
+net.derivative([3.14]) for just trained sinus network
+we obtain %s (cosinus at 3.14 is -1).
+""" % net.derivative([3.14])
+ 
