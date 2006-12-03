abb33955b7e249bbf6c5ef19d47085defab9d4d7
mwojc <mwojc@552c7fd7-3522-0410-9f26-e0bcf82f944f>
2006-12-03 15:21:51 +0000

    In ffnet.py: 1. Extended 'test' method, now data can be printed to file, there are three verbosity levels 2. Added examples folder and examples/xor.py example.

--- trunk/ffnet.py
+++ trunk/ffnet.py
@@ -245,6 +245,14 @@ class ffnet:
         # set initial normalization parameters
         self._setnorm()
         
+    def __repr__(self):
+        info = "Feed-forward neural network: \n" + \
+               "inputs:       %4i \n" %(len(self.inno)) + \
+               "hiddens:      %4i \n" %(len(self.hidno)) + \
+               "outputs:      %4i \n" %(len(self.outno)) + \
+               "connections:  %4i" %(len(self.conec)) 
+        return info
+    
     def __call__(self, inp):
         output = netprop.normcall(self.weights, self.conec, self.units, \
                                   self.inno, self.outno, self.eni, self.deo, inp)
@@ -553,35 +561,66 @@ class ffnet:
         self.weights = array(self.weights)
         self.trained = 'tnc'
         
-    def test(self, input, target, iprint = True):
+    def test(self, input, target, iprint = 1, filename = None):
         """
-        Calculates and prints parameters of regression line for targets vs. outputs.
-        Returns: (input, target, output, regress)
+        Calculates output and parameters of regression line of targets vs. outputs.
+        Returns: (output, regress)
         where regress contains regression line parameters for each output node. These
         parameters are (slope, intercept, r, two-tailed prob, stderr-of-the-estimate).
         
-        If iprint is True (default) regression info is printed to the screen.
+        Optional parameters:
+        iprint   - verbosity level:
+                   0 - print nothing
+                   1 - print regression parameters for each output node
+                   2 - print additionaly general network info and all targets vs. outputs
+                       (default is 1)
+        filename - string with path to the file; if given, output is redirected 
+                   to this file (default is None)
         """
+        # Check if we dump stdout to the file
+        if filename:
+            import sys
+            file = open(filename, 'w')
+            saveout = sys.stdout
+            sys.stdout = file
+        # Print network info
+        if iprint == 2:
+            print self
+            print
+        # Test data and get output
         input, target = self._testdata(input, target)
+        nump = len(input)
         output = array([self(inp) for inp in input])
-        
+        # Calculate regression info
         from scipy.stats import linregress
         numo = len(self.outno)
         target = target.transpose()
         output = output.transpose()
         regress = []
-        if iprint: print "*** Regression line parameters ***"
+        #if iprint: print "RESULTS FOR %i TESTING CASES" % nump
         for o in xrange(numo):
+            if iprint:
+                print "OUTPUT %i (node nr %i):" %(o+1, self.outno[o])
+            if iprint == 2:    
+                print "Targets vs. outputs:"
+                for p in xrange(nump):
+                    print "%4i % 13.6f % 13.6f" %(p+1, target[o,p], output[o,p])
             r = linregress(target[o], output[o])
             if iprint:
-                print "Output %i (node nr %i):" %(o, self.outno[o])
-                print "  slope       = %f" %(r[0])
-                print "  intercept   = %f" %(r[1])
-                print "  correlation = %f" %(r[2])
-                print "  tailprob    = %f" %(r[3])
-                print "  stderr      = %f" %(r[4])
+                print "Regression line parameters:"
+                print "slope       = % f" % r[0] 
+                print "intercept   = % f" % r[1]
+                print "correlation = % f" % r[2]
+                print "tailprob    = % f" % r[3]
+                print "stderr      = % f" % r[4]
             regress.append(r)
-        return input.tolist(), target.transpose().tolist(), output.transpose().tolist(), regress
+            if iprint: print
+        # Close file and restore stdout
+        if filename: 
+            file.close()
+            sys.stdout = saveout
+            
+        return output.transpose().tolist(), regress
 
 def savenet(net, filename):
     import cPickle
@@ -784,167 +823,4 @@ class TestFfnetSigmoid(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-        
-#~ class Testffnet(unittest.TestCase):
-    #~ def setUp(self):
-        #~ conec1 = [[1, 3], [2, 3], [0, 3] \
-                  #~ [1, 4], [2, 4], [0, 4] \
-                  #~ [3, 5], [4, 5], [0, 5]]
-        #~ arch1 = 2, 2, 1
-        #~ arch2 = 2, 1, 1
-        #~ input = [[0.,0.], [0.,1.], [1.,0.], [1.,1.]]
-        #~ target  = [[1.], [0.], [0.], [1.]]
 
-
-#~ conec = [[1, 3], [2, 3], \
-         #~ [1, 4], [2, 4], \
-         #~ [3, 5], [4, 5]]
-#~ conec = mlgraph((2,2,1))
-#~ conec = tmlgraph((2,1,1))
-#~ conec = [[1, 3], [2, 3], \
-         #~ [1, 4], [2, 4], [3, 4]]
-#print conec
-#~ conec = [[1, 3], [2, 3], \
-         #~ [1, 4], [2, 4], [3, 4]]
-#~ n = ffnet(conec=conec)
-#n.genetic.im_func.__doc__ += pikaia.__doc__
-#~ print n.conec
-#~ print n.inno
-#~ print n.outno
-#~ print n.bconec
-#~ print n.bconecno
-#~ print n.units
-#~ print n.weights
-#~ input = [[0.,0.], [0.,1.], [1.,0.], [1.,1.]]
-#~ target  = [[1.], [0.], [0.], [1.]]
-
-#~ target = netprop.vmapa(target, 0., 1., 0.15, 0.85)
-
-#~ #n.powell(input, target, maxiter=100)
-#~ #n.bfgs(input, target)
-#~ #n.anneal(input, target, lower = -1., upper = 1., learn_rate=0.001)
-#~ #n.genetic(input, target, lower = -5., upper = 5., individuals = 20, generations = 100)
-#~ #n.ncg(input, target, epsilon=0.0001)
-#~ #n.l_bfgs_b(input, target, iprint=1)
-#~ conec = mlgraph((2,4,2))
-#~ print conec
-#~ n = ffnet(mlp=(2,4,2))
-#~ print n.conec
-#~ print n.dconecno
-#~ from scipy import sin, pi, cos
-#~ pat = 20
-#~ #input = [[k*2*pi/pat] for k in xrange(1,pat+1)]+[[0.]]
-#~ #target = [[0.4*sin(x[0])+0.5] for x in input]
-
-#~ input = [[2*pi+k*2*pi/pat, k*2*pi/pat] for k in xrange(1,pat+1)]+[[2*pi,0.]]
-#~ target = [[2*sin(x[0]),5*cos(x[1])] for x in input]
-
-#~ #target = vmapa(target, -1., 1., 0.15, 0.85)
-#~ print n.weights
-#~ #n.genetic(input, target, lower = -20., upper = 20., individuals = 20, generations = 2000)
-
-#~ #n.train_tnc(input, target, maxfun=5000)
-#~ n.train_momentum(input, target, maxiter=10000)
-#~ #n.bfgs(input, target, iprint=1, bounds=n._getbounds())
-#~ #n.train_cg(input, target, maxiter=10000, disp=1)
-#~ print n.weights
-#~ print n.units
-#~ #n.genetic(input, target, lower = -5., upper = 5., individuals = 20, generations = 1000)
-#~ #print n.deriv([pi/6])
-
-#~ savenet(n, "lala")
-#~ n = loadnet("lala")
-
-
-#~ import pylab
-#~ inp = input[:-1]
-#~ inp1 = [x[0] for x in input[:-1]]
-#~ inp2 = [x[1] for x in input[:-1]]
-#~ res1 = [n(x)[0] for x in inp]
-#~ res2 = [n(x)[1] for x in inp]
-#~ #print target
-#~ #print res1
-#~ dres1 = [n.derivative(x)[0][0] for x in inp]
-#~ dres2 = [n.derivative(x)[1][0] for x in inp]
-#~ #from flagshyp import partial_derivative as pd
-#~ #res3 = [pd(n, [x], dx=0.001)[0] for x in inp]
-
-#~ pylab.plot(inp1, res1)
-#~ pylab.plot(inp1, dres1)
-#~ pylab.plot(inp2, res2)
-#~ pylab.plot(inp2, dres2)
-#~ #pylab.plot(inp, res3)
-#~ pylab.show()
-#~ #pylab.close()
-#~ #n.draw(biasnode=True)
-
-
-#~ bounds=n._getbounds()
-#~ def _getbounds(self):
-    #~ """Sets bounds of weights"""
-    #~ nofw = len(self.conec)
-    #~ bounds = []
-    #~ for w in xrange(nofw):
-        #~ trg = self.conec[w,1]
-        #~ n = len(self.graph.predecessors(trg))
-        #~ b = 100./n
-        #~ w = self.weights[w]
-        #~ if 0.8*b > w: upp = b
-        #~ else: upp = 1.2*w
-        #~ if -0.8*b < w: low = -b
-        #~ else: low = 1.2*w
-        #~ bounds += [(low, upp)]
-    #~ return bounds
-    
-    
-#~ def drawnet(self, biasnode = False):
-    #~ "Draws network in circular layout"
-    #~ try: import pylab
-    #~ except:
-        #~ print "Install matplotlib first"
-        #~ return
-    #~ pylab.close()
-    #~ pgraph = self.graph.copy()
-    #~ if not biasnode: pgraph.delete_node(0)
-    #~ NX.draw_circular(pgraph)
-    #~ pylab.show()
-    #~ return
-
-
-#~ print n.weights
-#~ for inp in input:
-    #~ print inp, n(inp)
-
-#~ from nn import ffnet, loadnet, savenet
-#~ # network initialization possibilities
-#~ n = ffnet(conec = conec)
-#~ n = ffnet(file = "conecfile") #reads connections
-#~ n = ffnet(mlp = arch)
-#~ n = ffnet(tmlp = arch)
-
-#~ n.train_genetic(input, target)
-#~ ans = n(inp)
-#~ deriv = n.derivative(inp)
-
-#~ loadnet("filename")
-#~ savenet(n, "filename")
-
-# nn module functions: loadnet, savenet
-# nn module classes: ffnet
-
-# ffnet attributes: inlimits, outlimits, trained, conec, weights, units
-# ffnet methods: call, derivative, train_*, plot
-
-
-    #~ def __getattr__(self, name):
-        #~ if name == 'genetic_data':
-            #~ return (self.conec, self.units, self.inno, self.outno)
-        #~ elif name == 'bprop_data':
-            #~ return (self.conec, self.bconecno, self.units, self.inno, self.outno)
-        #~ elif name == 'call_data':
-            #~ return (self.weights, self.conec, self.units, \
-                    #~ self.inno, self.outno, self.eni, self.deo)
-        #~ elif name == 'deriv_data':
-            #~ return (self.weights, self.conec, self.dconecno, self.dconecmk, \
-                    #~ self.units, self.inno, self.outno, self.eni, self.ded)
-    
