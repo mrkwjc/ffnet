bf3769cd95da14b2aa0d741e66dc06b46d3555f8
mwojc <mwojc@552c7fd7-3522-0410-9f26-e0bcf82f944f>
2007-03-22 10:55:50 +0000

    Updated README

--- trunk/__init__.py
+++ trunk/__init__.py
@@ -1,6 +1,40 @@
 '''
-Feed-forward neural network for python.
+ffnet is a fast and easy-to-use feed-forward neural 
+network training solution for python.
 
+Unique features present in ffnet:
+---------------------------------
+1. Any network connectivity without cycles is allowed.
+2. Training can be performed with use of several optimization 
+   schemes including: standard backpropagation with momentum, rprop,
+   conjugate gradient, bfgs, tnc, genetic alorithm based optimization.
+3. There is access to exact partial derivatives of network outputs 
+   vs. its inputs.
+4. Automatic normalization of data.
+
+Basic assumptions and limitations:
+----------------------------------
+1. Network has feed-forward architecture.
+2. Input units have identity activation function, 
+   all other units have sigmoid activation function.
+3. Provided data are automatically normalized, both input and output, 
+   with a linear mapping to the range (0.15, 0.85).
+   Each input and output is treated separately (i.e. linear map is 
+   unique for each input and output).
+4. Function minimized during training is a sum of squared errors 
+   of each output for each training pattern.
+   
+Performance
+-----------
+Excellent computational performance is achieved implementing core
+functions in fortran 77 and wrapping them with f2py. ffnet outstands
+in performance pure python training packages and is competitive to
+'compiled language' software. Moreover, a trained network can be 
+exported to fortran sources, compiled and called from many 
+programming languages.
+
+Usage
+-----
 Basic usage of the package is outlined below. 
 See description of ffnet module and its functions 
 (and especially ffnet class) for detailed explanations.
