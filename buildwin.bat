62ec3b9eabde0ededcb9452ec5008cb11c7268e0
Marek Wojciechowski <mrkwjc@users.noreply.github.com>
2015-02-16 00:06:00 +0000

    windows builder entries

--- buildwin.bat
+++ buildwin.bat
@@ -1,3 +1,12 @@
 python setup.py build --compiler=mingw32
 python setup.py bdist_wininst --skip-build
 python setup.py sdist
+
+REM c:\Anaconda\envs\py34\python.exe setup.py build --compiler=mingw32
+REM c:\Anaconda\envs\py34\python.exe setup.py bdist_wininst --skip-build
+
+REM c:\Anaconda\envs\py33\python.exe setup.py build --compiler=mingw32
+REM c:\Anaconda\envs\py33\python.exe setup.py bdist_wininst --skip-build
+
+REM c:\Anaconda\envs\py26\python.exe setup.py build --compiler=mingw32
+REM c:\Anaconda\envs\py26\python.exe setup.py bdist_wininst --skip-build
