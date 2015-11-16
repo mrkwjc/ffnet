fc2f579968353347f72dc5ddc18812eb3c08a151
Marek Wojciechowski <mrkwjc@users.noreply.github.com>
2015-11-16 13:07:21 +0000

    Added some icons

--- gui/bars.py
+++ gui/bars.py
@@ -6,59 +6,61 @@ from enthought.pyface.image_resource import ImageResource
 new_network_action = Action(
     name   = 'New...', 
     action = '_new',
-    image  = ImageResource( 'blue_ball'))
+    image  = ImageResource('document-new')
+    )
 
 load_network_action = Action(
     name   = 'Load...', 
     action = '_load',
-    image  = ImageResource( 'blue_ball'))
+    image  = ImageResource('document-open')
+    )
 
 save_as_network_action = Action(
     name   = 'Save...', 
     action = '_save_as',
-    image  = ImageResource( 'blue_ball'),
+    image  = ImageResource('document-save-as'),
     enabled_when = 'object.netlist')
 
 remove_network_action = Action(
     name   = 'Remove', 
     action = '_remove',
-    image  = ImageResource( 'blue_ball'),
+    image  = ImageResource('user-trash'),
     enabled_when = 'object.netlist')
 
 export_network_action = Action(
     name   = 'Export', 
     action = '_export',
-    image  = ImageResource( 'blue_ball'),
-    enabled_when = 'False')
+    image  = ImageResource('text-x-generic-template'),
+    enabled_when = 'True')
 
 input_data_action = Action(
     name   = 'Input', 
     action = '_load_input_data',
-    image  = ImageResource( 'blue_ball'),
+    image  = ImageResource('text-x-generic'),
     enabled_when = 'netlist')
 
 target_data_action = Action(
     name   = 'Target', 
     action = '_load_target_data',
-    image  = ImageResource( 'blue_ball'),
+    image  = ImageResource('text-x-generic'),
     enabled_when = 'object.netlist and object.input_data.filename')
 
 training_settings_action = Action(
     name   = 'Settings', 
     action = '_train_settings',
-    image  = ImageResource( 'blue_ball'),
+    image  = ImageResource('preferences-system'),
     enabled_when = 'object.input_data and object.target_data')
 
 train_action = Action(
     name   = 'Train!', 
     action = '_train',
-    image  = ImageResource( 'blue_ball'),
+    image  = ImageResource('go-next'),
     enabled_when = 'netlist and not object.train_algorithm.running.value')
 
 train_stop_action = Action(
     name   = 'Stop!', 
     action = '_train_stop',
-    image  = ImageResource( 'blue_ball'),
+    image  = ImageResource('media-record'),
     enabled_when = 'netlist and object.train_algorithm.running.value')
 
 
@@ -89,4 +91,5 @@ train_menu = Menu(train_actions,
 menubar = MenuBar(file_menu, train_menu)
 toolbar = ToolBar(network_actions,
                   data_actions,
-                  train_actions)
+                  train_actions,
+                  image_size=(22, 22))
