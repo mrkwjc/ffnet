772e37d3235e66d0912961459480647578c622d0
mwojc <mwojc@552c7fd7-3522-0410-9f26-e0bcf82f944f>
2015-12-17 11:11:09 +0000

    Added messages module

---
+++ trunk/gui/messages.py
@@ -0,0 +1,28 @@
+import pyface.api as pyface
+import traceback as tb
+import sys
+
+def display_error(message='Error occured!', title='Error', traceback = False):
+    msg = message
+    # Check if we have an error
+    etype, value, tbstr = sys.exc_info()
+    emsg = ''
+    if etype is not None:
+        if not traceback:
+            emsg = tb.format_exception_only(etype, value)
+        else:
+            emsg = tb.format_exception(etype, value, tbstr)
+        emsg = ''.join(emsg)
+        msg = msg + '\n\n' + emsg
+    # display error
+    pyface.error(None, msg, title=title)
+
+
+def display_confirm(message, title=None, cancel=False):
+    answer = pyface.confirm(None, message, title=title, cancel=cancel)
+    if answer == pyface.YES:
+        return True
+    if answer == pyface.NO:
+        return False
+    if answer == pyface.CANCEL:
+        return None
