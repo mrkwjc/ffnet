de0fa7a598da599b83e680cf8127fa6e58283fad
Marek Wojciechowski <mrkwjc@users.noreply.github.com>
2016-01-21 15:15:22 +0000

    Data dumping tested on windows

--- gui/dumper.py
+++ gui/dumper.py
@@ -14,7 +14,6 @@ import os
 from ffnet_import import *
 from messages import display_error
 
-
 class Dumper(HasTraits):
     training_data_only = Bool(False)
     validation_data_only = Bool(False)
@@ -23,9 +22,6 @@ class Dumper(HasTraits):
     target = Button
     output = Button
     output_derivatives = Button
-    app = Any
-    net = DelegatesTo('app')
-    data = DelegatesTo('app')
 
     def _training_data_only_changed(self, new):
         if new:
@@ -48,81 +44,90 @@ class Dumper(HasTraits):
             path = dialog.path
             try:
                 np.savetxt(path, out, newline = os.linesep)
-                self.app.logs.logger.info('%s saved to file: %s' %(sufix.capitalize().replace('-', ''), path))
+                self.app.logs.logger.info('%s saved to file: %s' %(sufix.capitalize().replace('-', ' '), path))
             except:
                 display_error("Error when saving data!")
                 return
 
     def _output_fired(self):
-        out = self.net.call(self.data.input)
+        net = self.app.net
+        data = self.app.data
+        out = net.call(data.input)
         sufix = 'output'
         if self.app.mode == 'train':
             if self.training_data_only:
-                out = out[~self.data.vmask]
+                out = out[~data.vmask]
                 sufix = 'training-' + sufix
             if self.validation_data_only:
-                out = out[self.data.vmask]
+                out = out[data.vmask]
                 sufix = 'validation-' + sufix
         if self.normalized_data:
-            out = ffnetmodule._normarray(out, self.net.eno)
+            out = ffnetmodule._normarray(out, net.eno)
             sufix = 'normalized-' + sufix
         self.dump(out, sufix)
 
     def _output_derivatives_fired(self):
-        out = self.net.derivative(inp)
+        net = self.app.net
+        data = self.app.data
+        out = net.derivative(data.input)
         s = out.shape
         out = out.reshape((s[0], s[1]*s[2]))
         sufix = 'output-derivatives'
         if self.app.mode == 'train':
             if self.training_data_only:
-                out = out[~self.data.vmask]
+                out = out[~data.vmask]
                 sufix = 'training-' + sufix
             if self.validation_data_only:
-                out = out[self.data.vmask]
+                out = out[data.vmask]
                 sufix = 'validation-' + sufix
         if self.normalized_data:
-            out = out/self.net.ded.flatten()
+            out = out/net.ded.flatten()
             sufix = 'normalized-' + sufix
         self.dump(out, sufix)
 
     def _input_fired(self):
-        out = self.data.input
+        net = self.app.net
+        data = self.app.data
+        out = data.input
         sufix = 'input'
         if self.app.mode == 'train':
             if self.training_data_only:
-                out = out[~self.data.vmask]
+                out = out[~data.vmask]
                 sufix = 'training-' + sufix
             if self.validation_data_only:
-                out = out[self.data.vmask]
+                out = out[data.vmask]
                 sufix = 'validation-' + sufix
         if self.normalized_data:
-            out = ffnetmodule._normarray(out, self.net.eni)
+            out = ffnetmodule._normarray(out, net.eni)
             sufix = 'normalized-' + sufix
         self.dump(out, sufix)
 
     def _target_fired(self):
-        out = self.data.target
+        net = self.app.net
+        data = self.app.data
+        out = data.target
         sufix = 'target'
         if self.app.mode == 'train':
             if self.training_data_only:
-                out = out[~self.data.vmask]
+                out = out[~data.vmask]
                 sufix = 'training-' + sufix
             if self.validation_data_only:
-                out = out[self.data.vmask]
+                out = out[data.vmask]
                 sufix = 'validation-' + sufix
         if self.normalized_data:
-            out = ffnetmodule._normarray(out, self.net.eno)
+            out = ffnetmodule._normarray(out, net.eno)
             sufix = 'normalized-' + sufix
         self.dump(out, sufix)
 
-    traits_view = View(Group(Item('training_data_only', visible_when="object.app.mode == 'train'"),
-                             Item('validation_data_only', visible_when="object.app.mode == 'train'"),
-                             Item('normalized_data')),
+    traits_view = View(
                        UItem('output'),
                        UItem('output_derivatives'),
                        UItem('input'),
-                       UItem('target', visible_when="object.app.mode in ['train', 'test'] and object.data.status==2"),
+                       UItem('target', visible_when="object.app.mode in ['train', 'test'] and object.app.data.status==2"),
+                       Group(Item('training_data_only', visible_when="object.app.mode == 'train'"),
+                             Item('validation_data_only', visible_when="object.app.mode == 'train'"),
+                             Item('normalized_data')),
                        resizable = True,
                        width = 0.15,
-                       buttons = ['OK', 'Cancel'],
+                       #buttons = ['OK', 'Cancel'],
                        title = 'Dumping data...')
