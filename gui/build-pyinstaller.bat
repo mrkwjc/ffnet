a903ee551c591088c9f164fce7e63b77cfbcf560
Marek Wojciechowski <mrkwjc@users.noreply.github.com>
2016-01-25 14:02:45 +0000

    updated builder

--- gui/build-pyinstaller.bat
+++ gui/build-pyinstaller.bat
@@ -1,2 +1,2 @@
- pyinstaller --noconsole --clean --additional-hooks-dir=hooks ffnetui.py
+ pyinstaller -F --noconsole --clean --additional-hooks-dir=hooks --icon=ffnetui128x128.ico ffnetui.py
 ::pyinstaller --noconsole --clean --onefile --additional-hooks-dir=hooks ffnetui.py
