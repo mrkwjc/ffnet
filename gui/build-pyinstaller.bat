f2d430f5a8d6841d0254bd31126dfe4b8571a8cb
Marek Wojciechowski <mrkwjc@users.noreply.github.com>
2016-01-27 10:08:55 +0000

    Added networkx hook for pyinstaller

--- gui/build-pyinstaller.bat
+++ gui/build-pyinstaller.bat
@@ -1,2 +1,1 @@
- pyinstaller --clean --noconfirm --additional-hooks-dir=hooks --icon=images/ffnetui128x128.ico ffnetui.py
-::pyinstaller --noconsole --clean --onefile --additional-hooks-dir=hooks ffnetui.py
+pyinstaller --clean --noconfirm --additional-hooks-dir=hooks --icon=images/ffnetui128x128.ico ffnetui.py
