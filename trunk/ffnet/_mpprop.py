d5ac83dbe4a38ab5199c59ec65d256fc2ca526bd
mwojc <mwojc@552c7fd7-3522-0410-9f26-e0bcf82f944f>
2016-02-10 23:37:38 +0000

    ffnet license switched to LGPL

--- trunk/ffnet/_mpprop.py
+++ trunk/ffnet/_mpprop.py
@@ -1,3 +1,10 @@
+########################################################################
+## Copyright (C) 2006 by Marek Wojciechowski
+## <mwojc@p.lodz.pl>
+##
+## Distributed under the terms of LGPL-3.0 license
+## https://opensource.org/licenses/LGPL-3.0
+########################################################################
 """
 Parallel training functions
 """
