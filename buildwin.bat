2825bdedfe7cac5fdf8bbab68b8f8bd493c627c4
Marek Wojciechowski <mrkwjc@users.noreply.github.com>
2016-02-11 20:36:48 +0000

    updated windows build script

--- buildwin.bat
+++ buildwin.bat
@@ -1,6 +1,12 @@
-python setup.py build --compiler=mingw32
-python setup.py bdist_wininst --skip-build
 python setup.py sdist
+python setup.py bdist_wininst
+
+REM ~ python setup.py sdist register upload
+REM ~ python setup.py bdist_wininst register upload
+
+REM ~ python setup.py build --compiler=mingw32
+REM ~ python setup.py bdist_wininst --skip-build
+REM ~ python setup.py sdist
 
 REM c:\Anaconda\envs\py34\python.exe setup.py build --compiler=mingw32
 REM c:\Anaconda\envs\py34\python.exe setup.py bdist_wininst --skip-build
