ce93a6dc2a600f773d1b911327467f7b8f8c0114
mwojc <mwojc@552c7fd7-3522-0410-9f26-e0bcf82f944f>
2016-02-15 17:53:33 +0000

    Added version file. Succesful build on win32

--- trunk/gui/about.py
+++ trunk/gui/about.py
@@ -11,17 +11,19 @@
 #from traits.etsconfig.api import ETSConfig
 #ETSConfig.toolkit = 'qt4'
 
+from version import version as ffnetui_version
+
 from pyface.image_resource import ImageResource
 from pyface.api import AboutDialog
 
 about = AboutDialog(parent = None,
                     image = ImageResource('ffnetui256x256'),
-                    additions = ['<b>ffnetui-0.8.1</b>',
+                    additions = ['<b>ffnetui-%s</b>' %ffnetui_version,
                                  'This is user interface for ffnet - ',
                                  'feed-forward neural network for python',
                                  '<a href=ffnet.sourceforge.net>http://ffnet.sourceforge.net</a>',
                                  '',
-                                 'Copyright &copy; 2011-2015', '<b>Marek Wojciechowski</b>',
+                                 'Copyright &copy; 2005-2016', '<b>Marek Wojciechowski</b>',
                                  'Technical University of Lodz, Poland',
                                  '<a href=mailto:mwojc@p.lodz.pl>mwojc@p.lodz.pl</a>',
                                  '',
